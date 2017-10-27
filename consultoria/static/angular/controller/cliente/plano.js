6d9fc80984a0804435082547d990bfb6451ac5c0
julliano <julliano@AGRO_SAT_014.AGROCONSULT.CORP>
2017-10-27 11:45:31 -0200

    feita logica para pagar caso tenha deixado para depois;

--- consultoria/static/angular/controller/cliente/plano.js
+++ consultoria/static/angular/controller/cliente/plano.js
@@ -3,12 +3,14 @@
 
 	angular.module("consultoria").controller("ClientePlanoController", ClientePlanoController);
 
-	ClientePlanoController.$inject = [ "$http", "LoginService", "Notification"];
+	ClientePlanoController.$inject = [ "$http", "LoginService", "Notification", "$window"];
 
 	/* @ngInject */
-	function ClientePlanoController($http, LoginService, Notification) {
+	function ClientePlanoController($http, LoginService, Notification, $window) {
 		var vm = this;
 		vm.usuario = LoginService.getUsuario();
+		vm.pagar = pagar;
+		vm.confere = confere;
 		
 		init();
 		
@@ -22,6 +24,14 @@
 				vm.vendas = response.data;
 			})
 		}
+		
+		function pagar(venda){
+			$window.open('https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code='+venda.pagamento.codigo)
+		}
+	
+		function confere(venda){
+			return venda.pagamento.status == 'aguardando pagamento'
+		} 
 		
 	}
 
