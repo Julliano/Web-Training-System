c3045b0c6698c7ec63cac608dcd01bcb038d15c9
julliano <julliano@AGRO_SAT_014.AGROCONSULT.CORP>
2017-11-16 14:17:03 -0200

    ajuste;

--- consultoria/controller/compra_controller.py
+++ consultoria/controller/compra_controller.py
@@ -22,11 +22,9 @@ class CompraController:
     
     def retornoPagSeguro(self):
         notificacao = request.json or request.form 
-        print('oi')
-        print(request.json)
         url = 'https://ws.pagseguro.uol.com.br/v3/transactions/notifications/%s?email=jullianovosorio@gmail.com&token=1DF98935374845F2B18992B39A1B8B0F' % notificacao['notificationCode']
         header = {'Content-Type': 'application/xml; charset=ISO-8859-1'}
-        response = requests.get(url, params=url, headers=header, verify=True, timeout=120)
+        response = requests.get(url, headers=header, verify=True, timeout=120)
         if response.status_code == 200:
             try:
                 resp = xmltodict.parse(response.content)
