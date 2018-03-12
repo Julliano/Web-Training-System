96d7afb6a32afe1ff4748719fa6c633b9430f03c
Julliano <Julliano@Julliano-PC>
2018-03-12 12:25:38 -0300

    ajuste nas datas de treinos futuros;

--- consultoria/controller/compra_controller.py
+++ consultoria/controller/compra_controller.py
@@ -66,11 +66,17 @@ class CompraController:
                         ultimaData = usu.ultimoTreino.data_entrega
                 if ultimaData is not None:
                     for treino in venda.treinos:
-                        treino.data_entrega = ultimaData + timedelta(days=(count*30)+32)
+                        if count == 0:
+                            treino.data_entrega = ultimaData + timedelta(days=(count*30)+32)
+                        else:
+                            treino.data_entrega = ultimaData + timedelta(days=(count*30)+30)
                         count += 1
                 else:
                     for treino in venda.treinos:
-                        treino.data_entrega = date.today() + timedelta(days=(count*30)+2)
+                        if count == 0:
+                            treino.data_entrega = date.today() + timedelta(days=(count*30)+2)
+                        else:
+                            treino.data_entrega = date.today() + timedelta(days=(count*30))
                         count += 1
                 db.session.add(venda)
                 db.session.commit()
